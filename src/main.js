4e14214a01af9cc9acdd72be24022721fe016c5b
Steven Kaufman <steveplaysguiola@gmail.com>
2018-04-25 10:53:30 -0500

    added main.js+boilerplate

---
+++ src/main.js
@@ -0,0 +1,18 @@
+function init(){
+
+    Render.start();
+    Update.start();
+}
+function render(){
+
+}
+function update(){
+    
+}
+
+function end(){
+    Render.stop();
+    Update.stop();
+}
+
+$(document).ready(init);
